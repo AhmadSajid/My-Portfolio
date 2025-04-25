94495705ac0a6b5ac68f5391f454f4b69f6ccbc4
Ahmad Sajid <ahmad.sajid605@gmail.com>
2025-04-25 23:30:13 +0500

    Update Header.js
    Updated Social links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -34,10 +34,10 @@ const Header = () =>  (
       </li>     
     </Div2>
     <Div3>
-      <SocialIcons href='https://github.com'>
+      <SocialIcons href='https://github.com/AhmadSajid'>
         <AiFillGithub size='3rem'/>
         </SocialIcons>
-      <SocialIcons href='https://linkedin.com'>
+      <SocialIcons href='https://www.linkedin.com/in/ahmad-sajid-7a9013153'>
         <AiFillLinkedin size='3rem'/>
         </SocialIcons>
       <SocialIcons href='https://instagram.com'>
